cefaf90b297bdabdf53b2720e758c4b71cfa7656
al-ameen244 <abdulrahmon@joinramp.co>
2025-09-30 18:34:04 +0100

    added lient notification and send update

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+const SocketIO = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+
+//init socket.io
+const io = SocketIO(server);
+
+app.use(express.static("public"));
+
+const users = new Set();
+
+io.on("connection", (socket) => {
+  console.log("New user connected");
+
+  socket.on("join", (userName) => {
+    users.add(userName);
+    // Notify all clients about the new user
+    io.emit("userJoined", userName);
+
+    // Send the updated user list to all clients
+    io.emit("userList", Array.from(users));
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
