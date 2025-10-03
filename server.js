f26576204ad2ab39a55a4a9fc22b909c6d38919b
al-ameen244 <abdulrahmon@joinramp.co>
2025-10-03 11:48:46 +0100

    added other feature

--- server.js
+++ server.js
@@ -23,6 +23,24 @@ io.on("connection", (socket) => {
 
     // Send the updated user list to all clients
     io.emit("userList", Array.from(users));
+
+    //handle incoming messages
+    socket.on("chatMessage", (message) => {
+      //broadcast the message to all clients
+      io.emit("chatMessage", { user: userName, text: message });
+    });
+
+    //handle user disconnect
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
+      users.forEach((user) => {
+        if (user === userName) {
+          users.delete(user);
+        }
+      });
+      // Send the updated user list to all clients
+      io.emit("userList", Array.from(users));
+    });
   });
 });
 
